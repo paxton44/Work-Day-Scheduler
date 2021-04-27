8842efedd71c61bcb979fd16aaaceeac54a2126f
Mitchell Flood <mitchflood1@gmail.com>
2021-04-27 15:42:31 -0600

    got the save button console logging and alerting in window. Next is to figure out local storage for the text input.

--- Assets/script.js
+++ Assets/script.js
@@ -23,5 +23,19 @@ var currentTime = moment().format('LT');
 console.log(currentTime);
 
 
-//Use jQuery to generate timeblocks
-var newDiv = $("<div>");
+
+//Making on click console log for save button using activity 5 as reference. 
+// This "document.ready" code isn't necessary in this example... but is useful to become familiar with.
+// "document.ready" makes sure that our JavaScript doesn't get run until the HTML document is finished loading.
+$(document).ready(function () {
+
+    // Here we use jQuery to select the header with "click-me" as its ID.
+    // Notice I have the #click-me, click, and then the function
+    // So $("#id|.class|element").on("action", function(){});
+    // And so whenever it is clicked...
+    $("#saveBtn").on("click", function () {
+        alert("I've been clicked!");
+        console.log("click");
+    });
+
+});
