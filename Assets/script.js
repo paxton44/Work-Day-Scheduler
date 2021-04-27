aa122414bdc966105dd5d7cfd7c63a04ecf665c6
Mitchell Flood <mitchflood1@gmail.com>
2021-04-27 16:39:34 -0600

    got the text area to save to local storage inside the on click function. now i need to create every time slot in the daytimer

--- Assets/script.js
+++ Assets/script.js
@@ -11,7 +11,7 @@ console.log("Connected to index.html");
 //Install moment using npm i moment --save
 //We are targeting the currentDay ID that is assigned to a <P> tag in the jumbotron
 //Make the Variable that uses moment to display the currentDay
-var currentDay = moment().format('MMMM Do YYYY');
+const currentDay = moment().format('MMMM Do YYYY');
 console.log(currentDay);
 
 //Crystal activity #8 shows how to print something to text by id 
@@ -19,12 +19,13 @@ console.log(currentDay);
 $("#currentDay").text(currentDay);
 
 //Show current time in the console using moment format LT from docs
-var currentTime = moment().format('LT');
+const currentTime = moment().format('LT');
 console.log(currentTime);
 
 
 
-//Making on click console log for save button using activity 5 as reference. 
+//Making on click console log for save button using activity 5 as reference.
+
 // This "document.ready" code isn't necessary in this example... but is useful to become familiar with.
 // "document.ready" makes sure that our JavaScript doesn't get run until the HTML document is finished loading.
 $(document).ready(function () {
@@ -34,8 +35,21 @@ $(document).ready(function () {
     // So $("#id|.class|element").on("action", function(){});
     // And so whenever it is clicked...
     $("#saveBtn").on("click", function () {
-        alert("I've been clicked!");
+        // checking if click works first
+        // alert("I've been clicked!");
         console.log("click");
+
+        // adding the save to local storage using activity 21 and activity 18 as an example. 
+        //This is how we make the text input able to be saved to local storage.
+        // this.siblings targets the text area and anything related to it that isn't a parent and allows the on click function to drive everything at once.
+        const timeBlock = $(this).siblings('textarea').attr('id');
+        const textValue = $(this).siblings('textarea').val()
+        localStorage.setItem(timeBlock, textValue);
+        console.log(textValue);
+        
+
+
+
     });
 
 });
